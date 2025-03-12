7db967a004b5bddc7799235dc9570912c8279af7
Kai Hudalla <kai.hudalla@bosch.com>
2025-03-12 18:41:00 +0100

    Add missing license headers

--- .github/scripts/check-3rd-party-licenses.sh
+++ .github/scripts/check-3rd-party-licenses.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+# ********************************************************************************
+#  Copyright (c) 2024 Contributors to the Eclipse Foundation
+#
+#  See the NOTICE file(s) distributed with this work for additional
+#  information regarding copyright ownership.
+#
+#  This program and the accompanying materials are made available under the
+#  terms of the Apache License Version 2.0 which is available at
+#  https://www.apache.org/licenses/LICENSE-2.0
+#
+#  SPDX-License-Identifier: Apache-2.0
+# *******************************************************************************/
+
 deps_file=${DEPS_FILE:-"DEPS.txt"}
 dash_jar=${DASH_JAR:-"/tmp/dash.jar"}
 dash_summary=${DASH_SUMMARY:-"DASH_SUMMARY.txt"}
